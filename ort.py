d85e6b3c432644fb50ee6edeab01071e8b165dd9
ColdNightHour <jdixo001@ucr.edu>
2017-02-14 22:02:01 -0800

    Got the slogan and obscenities to work. Now to remove duplicates and count frequencies in obscenities

--- ort.py
+++ ort.py
@@ -18,7 +18,7 @@ def loadDictionary(dictionary):
     with open('./dictionaries/' + dictionary + '.txt') as d:
         content = d.readlines()
     if dictionary == 'obscenities':
-        return dict( [(x.strip().split(',')[0], (x.strip().split(',')[1], x.strip().split(',')[1])) for x in content ])
+        return dict( [(x.strip().split(',')[0], (x.strip().split(',')[1], x.strip().split(',')[2])) for x in content ])
     else:
         return dict([ tuple(x.strip().split(',')) for x in content ])
 
@@ -26,40 +26,54 @@ def loadDictionary(dictionary):
 obscenities = loadDictionary('obscenities')
 slogans = loadDictionary('slogans')
 amplifiers = loadDictionary('amplifiers')
-#slogans = dict(slogans)
+
 #Gets the slogan
 #@Param: tweet text
-#Return: tuple --> (slogan ranking, plus how many slogans there are)
+#Return: tuple --> (slogan weight, plus how many slogans there are)
+def removeTargetDuplicates(body, target, list=None):
+    if list == None:
+        return body.replace(target, '')
+
+
 def sloganWeight(text):
     sloganWeights = []
     for slogan in slogans:
         if slogan in text:
             sloganWeights.append((100, text.count(slogan)))
-    return map(sum, zip(*sloganWeights))
+            text.replace(text, '')
+    return tuple(map(sum, zip(*sloganWeights)))
 
+def obscenityWeight(textList, amplifier=None):
+    return [obscenities[token] for token in textList if token in obscenities.keys()]
 
+def amplifierWeight(text):
+    return
+#Normalizes the text by stripping it of any emojis or non-alphanumeric characters
+#@Param: text to be normalized
+#@Return: list --> [normalized text tokes]
+def normalize(text):
+    normalizedText = text
+    if '\n' in text:
+        normalizedText = normalizedText.replace('\n', ' ')
+    return re.sub('[^A-Za-z 0-9]+', '', normalizedText).split(' ')
 
 #Performs the obscenity ranking
 #@Param: text to be ranked in obscenity
 #@Return: the ranking in obscenity of the text
 punctuation = ['.', '!', '?', ',', '\'', '\\']
-def rankText(text, slogans, amplifiers):
-    sWeights = 0
+def rankText(text, slogans=None, amplifiers=None):
+    sWeights = (0,0)
     aWeights = 0
     tWeights = 0
 
-    tokens = re.sub('[^A-Za-z 0-9]+', '', text).split(' ')
-    matches = [(token, closeMatches(token)) for token in tokens if closeMatches(token)]
-    if len(matches) == 0:
-        return 0.0
-    #This needs explanation. Essentially this takes the matched terms and puts them in a list (inner list comprehension)
-    #There are possible sublists in the list, where the outer list comprehension flattens the elements to be only individual
-    refinedMatches = [match for sublist in [confirmObscene(term) for term in matches if confirmObscene(term)] for match in sublist]
-    if len(refinedMatches) == 0:
-        return 0.0
-    termRankList = list(map(lambda word: dict(dictionary)[word], refinedMatches))
-    frequencySum = list(map(lambda freq: float(freq), dict(collections.Counter(refinedMatches)).values()))
-    weight = [float(term[0]) for term in termRankList]
+    if slogans != None:
+        sWeights = sloganWeight(text)
+    tokens = normalize(text)
+    return obscenityWeight(tokens)
+
+
+
+
 
 #print(rankText('dick ass fuck'))
 #print(rankText('I\'m so fucking mad right now. I need help'))
@@ -69,5 +83,6 @@ def rankText(text, slogans, amplifiers):
 #alaskan pipeline
 #d = dict([('sexy','')])
 #print('s' in d.itervalues())
-
-print sloganWeight('ass wipe black cock black cock')
+d = time.time()
+rankText('ass wipe black cock black cock', True, True)
+print(time.time() - d)
