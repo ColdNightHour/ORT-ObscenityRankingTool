7a57c320e6d3dba2fde546d48ebed50d22d3000e
ColdNightHour <jdixo001@ucr.edu>
2017-02-18 00:18:28 -0800

    Final testing before project, will fine tune in future when I take more time to do this.

--- ort.py
+++ ort.py
@@ -37,7 +37,8 @@ def sloganWeight(text):
             sloganWeights = sloganWeights + 100
             contributions = contributions + len(slogan.split(' '))
             amtSloganWeights = amtSloganWeights + 1
-
+    if amtSloganWeights == 0:
+        return (1,0)
     return (sloganWeights*(amtSloganWeights + 1), contributions)
 
 #Counts the amount of amplifiers in the text
@@ -45,8 +46,10 @@ def sloganWeight(text):
 #@Return: Integer --> Number of amplifiers
 def amplifierWeight(textList, multPerWord):
     m = ['multiplier' for token in textList if token in amplifiers.keys()]
+    if len(m) == 0:
+        return (1, 0)
     multipliers = {'multiplier' : m.count(mult) for mult in m}
-    return 1#(multPerWord*10*multipliers['multiplier'])(multipliers['multiplier'] + 1)
+    return ((multPerWord*10*multipliers['multiplier'])*(multipliers['multiplier'] + 1), multipliers['multiplier'])
 
 def obscenityWeight(textList):
     scores = [(obscenities[token][0], obscenities[token][1]) for token in textList if token in obscenities.keys()] #A list comprehension to make a list of values
@@ -55,6 +58,7 @@ def obscenityWeight(textList):
     multPerWord = 0;
     obscenityWeight = 0
     amtObscenityWeights = 0
+    print scores
 
     for score in scores:
         obscenityWeight = obscenityWeight + float(score[0])
@@ -67,7 +71,7 @@ def obscenityWeight(textList):
 #@Param: text to be normalized
 #@Return: list --> [normalized text tokes]
 def normalize(text):
-    normalizedText = text
+    normalizedText = text.lower()
     if '\n' in text:
         normalizedText = normalizedText.replace('\n', ' ')
     return re.sub('[^A-Za-z 0-9]+', '', normalizedText).split(' ')
@@ -76,28 +80,44 @@ def normalize(text):
 #@Param: text to be ranked in obscenity
 #@Return: the ranking in obscenity of the text
 punctuation = ['.', '!', '?', ',', '\'', '\\']
-def rankText(text, slogans=None, amplifiers=None):
-    sWeights = 0
-    oWeights = 0
-    aWeights = 0
+def rankText(text, slogans=True, amplifiers=True):
+    sWeights = (1,0)
+    oWeights = (1,0)
+    aWeights = (1,0)
 
     if slogans != None:
         sWeights = sloganWeight(text)
-        print sWeights
+
     tokens = normalize(text)
-    print obscenityWeight(tokens)
+    oWeights = obscenityWeight(tokens)
+
+    if amplifiers != None:
+        aWeights = amplifierWeight(tokens, oWeights[2])
+
+    print aWeights
+    print oWeights
+    print sWeights
+    contRatio = (sWeights[1] + oWeights[1] + aWeights[1])
+    contRatio = (contRatio * contRatio) / math.fabs(contRatio - len(tokens) + 1)
+    return math.log(aWeights[0]*oWeights[0]*sWeights[0]*contRatio, 10)
 
 
-
-
-
-#print(rankText('dick ass fuck'))
-#print rankText('long dick in my ass fuck', True, True)
-#print(rankText('I\'m so fucking mad right now. I need help'))
-#print(rankText('fuck the broncos, they can suck a dick'))
-#print(rankText('Fuck trump man, nigga is gay af'))
-#print(rankText('Did you cum in me!? Fuck you did huh you know what FUCK YOU fuck you fuck you, you fucking asshole.'))
 
 d = time.time()
-print rankText('sexy little lady, god damn', slogans=True, amplifiers=True)
+#print(rankText('dick ass fuck'))
+#print('\n')
+#print rankText('long dick in my ass fuck')
+#print('\n')
+#print(rankText('I\'m so fucking mad right now. I need help'))
+#print('\n')
+#print rankText('sexy little lady, god damn')
+#print('\n')
+print(rankText('fuck the broncos'))
+print('\n')
+print(rankText('Fuck trump man, nigga is gay af'))
+print('\n')
+print(rankText('Nudity is the way to go SON'))
+print('\n')
+#print(rankText('Did you cum in me!? Fuck you did huh you know what FUCK YOU fuck you fuck you, you fucking asshole.'))
+#print('\n')
 print(time.time() - d)
